c79a2ccba9250c9c40bfdbcd01c67c64c2fb0dc2
kingrayhan <rayhan095@gmail.com>
2018-04-21 17:47:54 +0600

    clean-up app.js

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
+/**
+ * Includes all required packages
+ */
 const express = require('express');
 const beautify = require('express-beautify');
 const pug = require('pug');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 // start express application
 const app = express();
 
-
+// Beautify rendered markups
 app.use(beautify());
 
+/**
+ * Enable body parser to get all form request
+ */
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/**
+ * Setup views
+ */
+// view directory
 app.set('views' , 'views');
+// setup view engine
 app.set('view engine' , 'pug');
 
-app.use(express.static('./public'));
 
-app.use('/'  , require('./routes'))
+/**
+ * Setup public directory for Client-side
+ */
+app.use(express.static( path.resolve(__dirname , './client') ));
 
 
+/**
+ * Routes
+ */
+app.use('/'  , require('./routes'));
 
+
+// done! we export it so we can start the site in start.js
 module.exports = app;
